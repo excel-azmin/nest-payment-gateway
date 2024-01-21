9ee0e9ed97a2d886b75ba7dfb2225b77e9fa1288
Shaid Azmin <92359442+excel-azmin@users.noreply.github.com>
2024-01-21 09:42:36 +0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { PaymentModule } from './payment/payment.module';
   imports: [
     PaymentModule,
     MongooseModule.forRoot(
-      'mongodb://root:mongorootPass2023@202.91.42.198:27018/nestJsPayment',
+      'mongodb://root:mongorootPass2023@localhost:27018/nestJsPayment',
       {
         authSource: 'admin',
       },
